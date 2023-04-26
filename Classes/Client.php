c2d03f26e53dbaf4a63216f3480e2afd233d4609
angalland <angalland@laposte.net>
2023-04-26 19:16:53 +0200

    ajout d'un € dans afficherReservation

--- Classes/Client.php
+++ Classes/Client.php
@@ -73,7 +73,7 @@ class Client {
             $total += ($reservation->dureeSejour() * $reservation->getChambre()->getPrix());
         }
             
-        return $result." Prix total : ".$total."</br></br>";
+        return $result." Prix total : ".$total." €</br></br>";
     }
         
 }
